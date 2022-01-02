b55fc57a85dc876ff4e4c59bdc3b41f75273f7a4
Jason Williams <jwilliamstap@gmail.com>
2022-01-02 15:05:26 -0500

    transfered html to js for SPA effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,97 @@
 const root = document.querySelector('#root');
 
+const homePage = () => {
+  return `
+    <div id="title-card">
+      <div id="title-card-content">
+        <h4>Hello, I'm</h4>
+        <h1 class="large-font">Jason <br> Williams</h1>
+        <div class="div-bar">
+        </div>
+        <h5>Software Engineer</h5>
+      </div>
+      <img 
+        id="headshot" 
+        src="assets/images/IMG_9151_cropped_1000px.jpg" 
+        alt="Jason Williams" 
+        class="img-circle" />
+    </div>
+    
+    <div id="about-me">
+      <div class="sub-card left">
+        <div>
+          <h2>About Me</h2>
+          <div class="div-bar"></div>
+          <p>
+            I am a software engineer looking to break into the software development industry. After 
+            completing the immersive coding bootcamp at Fullstack Academy in April 2021, It is now 
+            my passion to continue learning, growing, and taking on new challenges as a professional 
+            developer. I am seeking employment with a company that not only pushes the boundaries of 
+            technology but also inspires me to be the best software engineer I can be.
+          </p>
+        </div>
+      </div>
+      <div class="sub-card right">
+        <p>
+          Currently I am a lead technician at a security company based out of New Jersey. Here, I 
+          troubleshoot all different kinds of problems, whether it is related to programming, 
+          hardware, or connectivity, my purpose is to solve problems. I also relieve responsibilities 
+          of my direct manager and some roles in other departments by offering full independent 
+          service which includes designing, analyzing, building, and repairing systems from local 
+          system devices to the internet of things. In order to better serve customers and the 
+          company, I&#39;ve sought multiple certifications in Fire Alarm Systems and assorted 
+          intrusion and access control systems.
+        </p>
+      </div>
+    </div>
+    
+    <div id="contact">
+      <h1>Contact.... </h1>
+    </div>
+  `
+}
+
+const aboutPage = () => {
+  return `
+    <div id="about-me">
+      <div class="sub-card left">
+        <div>
+          <h2>About Me</h2>
+          <div class="div-bar"></div>
+          <p>
+            I am a software engineer looking to break into the software development industry. After 
+            completing the immersive coding bootcamp at Fullstack Academy in April 2021, It has become 
+            my passion to continue learning, growing, and taking on new challenges as a professional 
+            developer. I am seeking employment with a company that not only pushes the boundaries of 
+            technology but also inspires me to be the best software engineer I can be.
+          </p>
+        </div>
+      </div>
+      <div class="sub-card right">
+        <p>
+          Currently I am a lead technician at a security company based out of New Jersey. Here, I 
+          troubleshoot all different kinds of problems, whether it is related to programming, 
+          hardware, or connectivity, my purpose is to solve problems. I also relieve responsibilities 
+          of my direct manager and some roles in other departments by offering full independent 
+          service which includes designing, analyzing, building, and repairing systems from local 
+          system devices to the internet of things. In order to better serve customers and the 
+          company, I&#39;ve sought multiple certifications in Fire Alarm Systems and assorted 
+          intrusion and access control systems.
+        </p>
+      </div>
+    </div>
+  `
+}
+
+const contactPage = () => {
+  return `
+    <div class="div-bar"></div>
+    <div class="div-bar"></div>
+    <div class="div-bar"></div>
+    <div class="div-bar"></div>
+  `
+}
+
 let hash = '';
 
 window.addEventListener('hashchange', ()=> {
@@ -8,123 +100,17 @@ window.addEventListener('hashchange', ()=> {
     hash = curHash;
   }
   if (hash === 'about') {
-    return root.innerHTML = `
-      <div id="about-me">
-        <div class="sub-card left">
-          <div>
-            <h2>About Me</h2>
-            <div class="div-bar"></div>
-            <p>
-              I am a software engineer looking to break into the software development industry. After 
-              completing the immersive coding bootcamp at Fullstack Academy in April 2021, It has become 
-              my passion to continue learning, growing, and taking on new challenges as a professional 
-              developer. I am seeking employment with a company that not only pushes the boundaries of 
-              technology but also inspires me to be the best software engineer I can be.
-            </p>
-          </div>
-        </div>
-        <div class="sub-card right">
-          <p>
-            Currently I am a lead technician at a security company based out of New Jersey. Here, I 
-            troubleshoot all different kinds of problems, whether it is related to programming, 
-            hardware, or connectivity, my purpose is to solve problems. I also relieve responsibilities 
-            of my direct manager and some roles in other departments by offering full independent 
-            service which includes designing, analyzing, building, and repairing systems from local 
-            system devices to the internet of things. In order to better serve customers and the 
-            company, I&#39;ve sought multiple certifications in Fire Alarm Systems and assorted 
-            intrusion and access control systems.
-          </p>
-        </div>
-      </div>
-    `
+    return root.innerHTML = aboutPage();
   }
   if (hash === 'contact') {
-    console.log('contact')
+    return root.innerHTML = contactPage();
   }
   else {
-    root.innerHTML = `
-      <div id="title-card">
-        <div id="title-card-content">
-          <h4>Hello, I'm</h4>
-          <h1 class="large-font">Jason <br> Williams</h1>
-          <div class="div-bar">
-          </div>
-          <h5>Software Engineer</h5>
-        </div>
-        <img 
-          id="headshot" 
-          src="assets/images/IMG_9151_cropped_1000px.jpg" 
-          alt="Jason Williams" 
-          class="img-circle" />
-      </div>
-      
-      <div id="about-me">
-        <div class="sub-card left">
-          <div>
-            <h2>About Me</h2>
-            <div class="div-bar"></div>
-            <p>
-              I am a software engineer looking to break into the software development industry. After 
-              completing the immersive coding bootcamp at Fullstack Academy in April 2021, It is now 
-              my passion to continue learning, growing, and taking on new challenges as a professional 
-              developer. I am seeking employment with a company that not only pushes the boundaries of 
-              technology but also inspires me to be the best software engineer I can be.
-            </p>
-          </div>
-        </div>
-        <div class="sub-card right">
-          <p>
-            Currently I am a lead technician at a security company based out of New Jersey. Here, I 
-            troubleshoot all different kinds of problems, whether it is related to programming, 
-            hardware, or connectivity, my purpose is to solve problems. I also relieve responsibilities 
-            of my direct manager and some roles in other departments by offering full independent 
-            service which includes designing, analyzing, building, and repairing systems from local 
-            system devices to the internet of things. In order to better serve customers and the 
-            company, I&#39;ve sought multiple certifications in Fire Alarm Systems and assorted 
-            intrusion and access control systems.
-          </p>
-        </div>
-      </div>
-      
-      <div id="contact">
-        <h1>Contact.... </h1>
-      </div>
-    `
+    root.innerHTML = homePage();
   }
 })
 
 
-// table.addEventListener('mousedown', (ev)=> {
-//   let active = true;
-//     if (ev.target.tagName === 'TD' && active) ev.target.className = selColor;
-//   table.addEventListener('mouseover', (ev)=> {
-//     if (ev.target.tagName === 'TD' && active) ev.target.className = selColor;
-//   })
-//   table.addEventListener('mouseup', (ev)=> {
-//     active = false;
-//   })
-// })
 
-// const makeRow = () => {
-//   const newRow = document.createElement('tr');
-//   const pixel = document.createElement('td');
-  
-//   newRow.innerHTML = `
-//     <td></td>
-//     <td></td>
-//     <td></td>
-//   `
-//   table.appendChild(newRow);
-// }
+root.innerHTML = homePage();
 
-// const button = document.querySelector('button');
-// button.addEventListener('click', ()=> {
-//   makeRow();
-// })
-
-// const colorSelector = document.querySelector('select');
-// let selColor = '';
-
-// colorSelector.addEventListener('change', (ev)=> {
-//   selColor = ev.target.value;
-// })
